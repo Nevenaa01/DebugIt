afa0b6b3f341bdbdbc619f17392ac06401ff0b49
Nevena Gligorov <114764987+Nevenaa01@users.noreply.github.com>
2024-08-14 19:56:00 +0200

    feat: navbar

--- frontend/DebugIt/src/app/app-routing.module.ts
+++ frontend/DebugIt/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllQuestionsComponent } from './feature-modules/all-questions/all-questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AllQuestionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/DebugIt/src/app/app.module.ts
+++ frontend/DebugIt/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './feature-modules/navbar/navbar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { AllQuestionsComponent } from './feature-modules/all-questions/all-questions.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AllQuestionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
